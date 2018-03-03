0fdf2b2ccadca2dd5c9e710f452058a9f5c65cbf
Kennard Ng <Kennard_ng@outlook.com>
2018-03-03 23:16:21 +0800

    Add session.php

---
+++ session.php
@@ -0,0 +1,17 @@
+<?php
+	session_start();
+
+	function logout() {
+		if (session_status() == PHP_SESSION_ACTIVE) {
+			session_destroy();
+		}
+	}
+
+	function login($username) {
+		$_SESSION['user'] = $username;
+	}
+
+	function isLoggedIn() {
+		return isset($_SESSION['user']);
+	}
+?>
