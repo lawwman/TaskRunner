3853fb850dd230fa1eb11326cd08657266e9c699
Kennard Ng <Kennard_ng@outlook.com>
2018-03-05 21:55:36 +0800

    Add encryption to signup.php

--- signup.php
+++ signup.php
@@ -36,7 +36,7 @@
     $emailError = "";
     if (isUserUnique($nameError, $emailError)) {
       $username = $_POST['username'];
-      $password = $_POST['password'];
+      $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
       $email = $_POST['email'];
       $firstName = $_POST['first-name'];
       $lastName = $_POST['last-name'];
