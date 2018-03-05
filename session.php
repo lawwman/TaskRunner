878595020ceea2d9d2f6d4ec9f6a7aeae24b6069
Kennard Ng <Kennard_ng@outlook.com>
2018-03-05 19:39:15 +0800

    Redirect users from addtasks.php to login.php if they have not signed in.

--- session.php
+++ session.php
@@ -1,6 +1,9 @@
 <?php
   session_start();
 
+  /**
+   * Logs the current user out. 
+   */
   function logout() {
     if (session_status() == PHP_SESSION_ACTIVE) {
       session_unset();
@@ -8,11 +11,26 @@
     }
   }
 
+  /**
+   * Logs into param $username account. 
+   */
   function login($username) {
     $_SESSION['user'] = $username;
   }
 
+  /**
+   * Returns true if user is logged in.
+   */
   function isLoggedIn() {
     return isset($_SESSION['user']);
+  }
+
+  /**
+   * Redirects to login page if user is not logged in.
+   */
+  function redirectToLogin() {
+    if (!isLoggedIn()) {
+      header('Location: /demo/login.php');
+    }
   }
 ?>
