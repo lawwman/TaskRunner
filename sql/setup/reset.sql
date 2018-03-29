78403d9e8e442ba3b96285729c3101255eeae23e
LawMan <lawrenciok@gmail.com>
2018-03-29 23:48:34 +0800

    add new sql folder

--- sql/setup/reset.sql
+++ sql/setup/reset.sql
@@ -6,7 +6,6 @@ DROP TABLE IF EXISTS Taskees CASCADE;
 DROP TABLE IF EXISTS Taskers CASCADE;
 DROP TABLE IF EXISTS Skills CASCADE;
 DROP TABLE IF EXISTS HasSkills CASCADE;
-DROP TABLE IF EXISTS Taskss CASCADE;
 
 CREATE TABLE Taskees (
     email VARCHAR(100) PRIMARY KEY,
@@ -62,52 +61,24 @@ CREATE TABLE HasSkills (
     PRIMARY KEY (tEmail, sname)
 );
 
-CREATE TABLE Taskss (
-    createdAt DATE,
-    taskeeEmail VARCHAR(100) REFERENCES Taskees,
-    taskerEmail VARCHAR(100) REFERENCES Taskers,
-   	
-    ttype VARCHAR(50) REFERENCES Skills,
-    startDate DATE NOT NULL,
-    startTime TIME NOT NULL,
-    details TEXT NOT NULL,
-    loc VARCHAR(100) NOT NULL,
-    duration VARCHAR(100) NOT NULL 
-);
-
-CREATE TABLE Users (
-	username VARCHAR(20) PRIMARY KEY,
-	user_pw TEXT NOT NULL,
-	user_firstname VARCHAR(50) NOT NULL,
-	user_lastname VARCHAR(50) NOT NULL,
-	email VARCHAR(100) UNIQUE NOT NULL,
-	contact VARCHAR(22),	
-	occupation VARCHAR(50) NOT NULL,
-	birth_date DATE NOT NULL,
-
-  -- we can't have someone born later than today using this interface
-	CHECK(birth_date < now())
-);
-
 CREATE TABLE Tasks (
 	task_id BIGINT PRIMARY KEY, -- BIGINT so that can contain more than 4 bil accounts (in case single user creates multiple accs)
-	task_name VARCHAR(100), -- Task name should be short and sweet 
+    ttype VARCHAR(50) NOT NULL REFERENCES Skills,
 	task_details VARCHAR(3000), -- Details of task 
 	
-	duration_minutes INT, 
+    taskeeEmail VARCHAR(100) REFERENCES Taskees,
+    taskerEmail VARCHAR(100) NOT NULL REFERENCES Taskers,
 
-	creator VARCHAR(100) NOT NULL references Users,
-	runner VARCHAR(100) references Users,
-	
-	reward DECIMAL NOT NULL DEFAULT 0.0,
 	status VARCHAR(100) DEFAULT 'not bidded',
 
 	createdDateTime TIMESTAMP NOT NULL DEFAULT now(), -- date that task was created
+    startDateTime TIMESTAMP NOT NULL, -- start of task (include date and time)
+    endDateTime TIMESTAMP NOT NULL, -- end of task (include date and time)
 
-	UNIQUE (task_name, task_id),
+    loc VARCHAR(100) NOT NULL,
 
-  CHECK ((runner IS DISTINCT FROM NULL AND status <> 'not bidded' AND status <> 'bidded') 
-           OR (runner IS NOT DISTINCT FROM NULL AND (status = 'not bidded' OR status = 'bidded' OR status = 'deleted'))),
+  CHECK ((taskeeEmail IS DISTINCT FROM NULL AND status <> 'not bidded' AND status <> 'bidded') 
+           OR (taskeeEmail IS NOT DISTINCT FROM NULL AND (status = 'not bidded' OR status = 'bidded' OR status = 'deleted'))),
            
 	-- 'pending completion' refers to tasks that have been accepted
 	-- 'not bidded' refers to instance when there is no bidders for the current task yet.  
@@ -118,27 +89,19 @@ CREATE TABLE Tasks (
 
 CREATE TABLE Bids (
 	task_id BIGINT NOT NULL REFERENCES Tasks, 
-	bidder_name VARCHAR(20) NOT NULL REFERENCES Users,
-	creator_name VARCHAR(20) NOT NULL REFERENCES Users,
+    taskeeEmail VARCHAR(100) NOT NULL REFERENCES Taskees,
+    taskerEmail VARCHAR(100) NOT NULL REFERENCES Taskers,
 	
   status VARCHAR(100) NOT NULL DEFAULT 'pending', 
 	bidDateTime TIMESTAMP NOT NULL DEFAULT now(),
 
   CONSTRAINT discrete_status CHECK(status in ('rejected', 'pending', 'accepted')),
-	PRIMARY KEY(bidder_name, creator_name, task_id)
+	PRIMARY KEY(taskerEmail, taskeeEmail, task_id)
 );
 
-COPY Skills FROM '..\..\apps\demo\htdocs\oldSQL\setup\data\skills.csv' DELIMITER ',' CSV HEADER;
-COPY Taskees FROM '..\..\apps\demo\htdocs\oldSQL\setup\data\taskees.csv' DELIMITER ',' CSV HEADER;
-COPY Taskers FROM '..\..\apps\demo\htdocs\oldSQL\setup\data\taskers.csv' DELIMITER ',' CSV HEADER;
-COPY HasSkills FROM '..\..\apps\demo\htdocs\oldSQL\setup\data\hasskills.csv' DELIMITER ',' CSV HEADER;
-COPY Taskss FROM '..\..\apps\demo\htdocs\sql\oldSQL\data\taskss.csv' DELIMITER ',' CSV HEADER;
-
-
-
-
-
-COPY Users FROM '..\..\apps\demo\htdocs\oldSQL\setup\data\users.csv' DELIMITER ',' CSV HEADER;
-COPY Tasks FROM '..\..\apps\demo\htdocs\oldSQL\setup\data\tasks.csv' DELIMITER ',' CSV HEADER;
-COPY Bids FROM '..\..\apps\demo\htdocs\oldSQL\setup\data\bids.csv' DELIMITER ',' CSV HEADER;
-
+COPY Skills FROM '..\..\apps\demo\htdocs\sql\setup\data\skills.csv' DELIMITER ',' CSV HEADER;
+COPY Taskees FROM '..\..\apps\demo\htdocs\sql\setup\data\taskees.csv' DELIMITER ',' CSV HEADER;
+COPY Taskers FROM '..\..\apps\demo\htdocs\sql\setup\data\taskers.csv' DELIMITER ',' CSV HEADER;
+COPY HasSkills FROM '..\..\apps\demo\htdocs\sql\setup\data\hasskills.csv' DELIMITER ',' CSV HEADER;
+COPY Tasks FROM '..\..\apps\demo\htdocs\sql\setup\data\tasks.csv' DELIMITER ',' CSV HEADER;
+COPY Bids FROM '..\..\apps\demo\htdocs\sql\setup\data\bids.csv' DELIMITER ',' CSV HEADER;
