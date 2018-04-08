635380d63cf9ca6a9b263974a9876e96473501b5
LawMan <lawrenciok@gmail.com>
2018-04-08 15:53:39 +0800

    add billing field

--- edittaskeeprofile.php
+++ edittaskeeprofile.php
@@ -249,6 +249,44 @@
   </div>
 
   <div class="ui bottom attached tab segment" data-tab="third">
+    <form class="ui form billing" action="/demo/edittaskeeprofile.php" method="POST" >
+      <h4 class="ui dividing header">Billing Information</h4>
+      <div class="fields">
+        <div class="seven wide field">
+          <label>Card Number</label>
+          <input type="text" name="creditNum" maxlength="16" placeholder="Card Number">
+        </div>
+        <div class="three wide field">
+          <label>CVC</label>
+          <input type="text" name="creditSecurity" maxlength="3" placeholder="CVC">
+        </div>
+        <div class="six wide field">
+          <label>Expiration</label>
+          <div class="two fields">
+            <div class="field">
+              <select class="ui fluid search dropdown" name="expiryMonth">
+                <option value="">Month</option>
+                <option value="1">January</option>
+                <option value="2">February</option>
+                <option value="3">March</option>
+                <option value="4">April</option>
+                <option value="5">May</option>
+                <option value="6">June</option>
+                <option value="7">July</option>
+                <option value="8">August</option>
+                <option value="9">September</option>
+                <option value="10">October</option>
+                <option value="11">November</option>
+                <option value="12">December</option>
+              </select>
+            </div>
+            <div class="field">
+              <input type="text" name="expiryYear" maxlength="4" placeholder="Year">
+            </div>
+          </div>
+        </div>
+      </div>
+    </form>
   </div>
 </div>
 </body>
