075099e8fdd622393eab3243fb548496b4a73ca3
nahtanojmil <jonlim33@gmail.com>
2018-04-12 17:13:42 +0800

    sanitize input for admin new skills

--- addnewskillstodb.php
+++ addnewskillstodb.php
@@ -1,13 +1,13 @@
 <?php
-	//require('sanitize.php');
+	require('sanitize.php');
 	require('session.php');
 
 if(isset($_POST['newskill']) && isset($_POST['newskilldesc'])) {
 	$db = pg_connect("host=127.0.0.1 port=5432 dbname=project1 user=postgres password=1234") or die('Could not connect: ' . pg_last_error());
 		$newskill = json_decode($_POST['newskill']);
-		//$newskill = getValidString($newskill);
+		$newskill = getValidString($newskill);
 		$newskilldesc = json_decode($_POST['newskilldesc']);
-		//$newskilldesc = getValidString($newskilldesc);
+		$newskilldesc = getValidString($newskilldesc);
 		$insertQueryAdmin = "INSERT INTO skills VALUES ('$newskill', '$newskilldesc') ";
 		$result = pg_query($db, $insertQueryAdmin);
       	if ($result) {
